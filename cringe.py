b96cf87d57fd84eb8cc754e3f3b17c14bda0187f
Rishab Ramanathan <rrishab2001@gmail.com>
2020-07-27 00:43:59 +0530

    fuck rate limits

--- cringe.py
+++ cringe.py
@@ -19,7 +19,15 @@ class MyStreamListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self,tweet):
-        print(f"{tweet.user.name}:{tweet.text}")
+        try:
+            if "#cringe" in tweet.text.lower():
+                print(f"#cringe found in {tweet.text}, replying")
+                api.update_status("@bottestingOwO cringe",tweet.id)
+                print("success")
+            else:
+                print(f"{tweet.user.name} \t {tweet.text}")
+        except:
+            print("failure")
     
     def on_error(self,status):
         if status == 420: #nice
@@ -41,5 +49,4 @@ api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
 #tweet listener
 tweets_listener = MyStreamListener(api)
 stream = tweepy.Stream(api.auth, tweets_listener)
-stream.filter(track=["1284123616682991617"])
-
+stream.filter(follow = ["1284123616682991617"],languages = ["en"])
