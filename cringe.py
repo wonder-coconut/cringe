e6c46e8315e76a7fea7056c9de2ad9a6293330f7
Rishab Ramanathan <rrishab2001@gmail.com>
2020-07-27 12:27:55 +0530

    i pray replying works now

--- cringe.py
+++ cringe.py
@@ -19,16 +19,18 @@ class MyStreamListener(tweepy.StreamListener):
         self.me = api.me()
 
     def on_status(self,tweet):
-        try:
-            if "#cringe" in tweet.text.lower():
-                print(f"#cringe found in {tweet.text}, replying")
-                api.update_status("@bottestingOwO cringe",tweet.id)
-                print("success")
-            else:
-                print(f"{tweet.user.name} \t {tweet.text}")
+        try:#replying to tweets with the hashtag cringe
+            if "#cringe" in tweet.text:
+                print(f"cringe found, replying to tweet id:{tweet.id}")
+                api.update_status(status = f"@{tweet.user.screen_name} [bot] i have found cringe",in_reply_to_status_id = tweet.id)
+                print(f"cringe replied")
+
+            else:#printing live update tweets from my bot testing account
+                print(f"{tweet.user.screen_name}\tsaid\t{tweet.text}")
+
         except:
-            print("failure")
-    
+            print("error")
+
     def on_error(self,status):
         if status == 420: #nice
             print("rate limit exceeded,killing bot")
@@ -44,7 +46,7 @@ auth = tweepy.OAuthHandler(getToken(0),getToken(1))
 auth.set_access_token(getToken(2),getToken(3))
 
 #api object
-api = tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+api = tweepy.API(auth)
 
 #tweet listener
 tweets_listener = MyStreamListener(api)
